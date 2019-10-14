a55cb59d09d0416bbf864c306ea218353d1c6bfa
ivoelbert <ivo.elbert1@gmail.com>
2019-10-14 12:19:39 -0300

    Fix routing for github pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
 
 const routes: Routes = [
     {
         path: 'shorten-app',
-        component: AppComponent,
+        redirectTo: '/'
     },
 ];
 
